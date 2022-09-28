04da973a7877feab15016778081efb36f1ed658a
MohamedIbnSalah <114621229+MohamedIbnSalah@users.noreply.github.com>
2022-09-29 00:02:11 +0200

    Add files via upload

---
+++ AutoSave/AutoSave/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Tahoma", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 39
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(734, 394)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Tahoma", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(475, 412)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(271, 61)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "تحديث القائمة 🔄"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("Tahoma", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"الحفظ كل 30 ثانية", "الحفاظ كل 60 ثانية", "الحفظ كل 120 ثانية", "الحفظ كل 180 ثانية"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 414)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ComboBox1.Size = New System.Drawing.Size(442, 56)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 30000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(26, 480)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(701, 58)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "إختار الإعدادات المطلوبة وقم بتصغير البرنامج وسوف يعمل في الخلفية" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "نسألكم الدعاء " & _
+            "بالشفاء لى ولجميع المسلمين"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(26, 544)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label2.Size = New System.Drawing.Size(701, 34)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "أخر عملية حفظ تمت بتاريخ : ###########"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(758, 587)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(780, 643)
+        Me.MinimumSize = New System.Drawing.Size(780, 643)
+        Me.Name = "Form1"
+        Me.Text = "Auto Save"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ AutoSave/AutoSave/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ListBox1.Focus()
+        ListBox1.Items.Clear()
+        For Each p As Process In Process.GetProcesses
+            If p.MainWindowTitle <> "" Then
+                ListBox1.Items.Add(p.MainWindowTitle)
+            End If
+        Next
+        ListBox1.SelectedIndex = -1
+        ListBox1.Focus()
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex = 0 Then
+            Timer1.Interval = 30000
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            Timer1.Interval = 60000
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            Timer1.Interval = 120000
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            Timer1.Interval = 180000
+        End If
+    End Sub
+
+    <DllImport("user32.dll", EntryPoint:="GetWindowThreadProcessId")> _
+    Private Shared Function GetWindowThreadProcessId(<InAttribute()> ByVal hWnd As IntPtr, ByRef lpdwProcessId As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll", EntryPoint:="GetForegroundWindow")> Private Shared Function GetForegroundWindow() As IntPtr
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> Private Shared Function GetWindowTextLength(ByVal hwnd As IntPtr) As Integer
+    End Function
+
+    <DllImport("user32.dll", EntryPoint:="GetWindowTextW")> _
+    Private Shared Function GetWindowTextW(<InAttribute()> ByVal hWnd As IntPtr, <OutAttribute(), MarshalAs(UnmanagedType.LPWStr)> ByVal lpString As StringBuilder, ByVal nMaxCount As Integer) As Integer
+    End Function
+
+    Private Function GetForgroundWindowInfo() As String
+        Dim hWnd As IntPtr = GetForegroundWindow()
+        If Not hWnd.Equals(IntPtr.Zero) Then
+
+            Dim lgth As Integer = GetWindowTextLength(hWnd)
+            Dim wTitle As New System.Text.StringBuilder("", lgth + 1)
+            If lgth > 0 Then
+                GetWindowTextW(hWnd, wTitle, wTitle.Capacity)
+            End If
+
+
+            Return wTitle.ToString
+        End If
+        Return ""
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If ListBox1.SelectedIndex > -1 Then
+            If ListBox1.Text = GetForgroundWindowInfo() Then
+                Label2.Text = "أخر عملية حفظ تمت بتاريخ : " & Now
+                SendKeys.SendWait("^{s}")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        ListBox1.Focus()
+        ListBox1.Items.Clear()
+        For Each p As Process In Process.GetProcesses
+            If p.MainWindowTitle <> "" Then
+                ListBox1.Items.Add(p.MainWindowTitle)
+            End If
+        Next
+        ListBox1.SelectedIndex = -1
+        ListBox1.Focus()
+    End Sub
+End Class
